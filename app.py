8e98f9fcd95795adc4611a5fe6edbab9d4aa8bab
Ayça Aksoy <aycaaksoy2000@gmail.com>
2023-05-25 19:58:48 +0300

    filtering works (multiple countries CANNOT be chosen)

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
 import pandas as pd
 from gower import gower_matrix
+import json
 
 app = Flask(__name__)
 
 # Load your survey data into a pandas DataFrame
-data = pd.read_csv("260Datav2.csv")
+data = pd.read_csv("C:/Users/aycaa/PycharmProjects/MastersFinder/260Datav2.csv")
+merged_data = pd.read_csv("C:/Users/aycaa/PycharmProjects/MastersFinder/merged_data.csv")
 
-
-# uniData = pd.read_csv("merged_data.csv")
+# Convert the merged_data DataFrame to a JSON object
+merged_data_json = json.loads(merged_data.to_json(orient='records'))
+unique_countries = merged_data['Country'].unique().tolist()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    merged_data_for_index = merged_data[["University", "Country"]]
+    return render_template('index.html', merged_data=merged_data_json, unique_countries=unique_countries)
 
 
 @app.route('/recommendation', methods=['POST'])
@@ -29,7 +33,18 @@ def recommendation():
     q6 = int(request.form['q6'])
     q7 = int(request.form['q7'])
 
-    # Prepare the user input as a dictionary
+    # Get the selected countries from the form
+    selected_countries = request.form.getlist('country')
+
+    # Filter the merged_data based on the selected countries
+    if selected_countries:
+        merged_data_filtered = merged_data[merged_data['Country'].isin(selected_countries)]
+    else:
+        merged_data_filtered = merged_data
+
+    # Prepare the filtered_data for further processing or usage
+    filtered_data = merged_data_filtered.copy()
+
     user_input = {
         "Hangi bölümde okuyorsunuz? LİSANS": major,
         "Hangi ünide okuyorsunuz? LİSANS": university,
@@ -48,7 +63,6 @@ def recommendation():
     # Concatenate the user input DataFrame to the existing dataset
     data_with_input = pd.concat([data, user_input_df], ignore_index=True)
 
-    # Prepare the data for Gower's distance calculation
     categorical_cols = ["Hangi bölümde okuyorsunuz? LİSANS", "Hangi ünide okuyorsunuz? LİSANS"]
     numerical_cols = ["Üniversitenin Dünya Sıralaması", "Üniversitenin Akademik Bilinirliği",
                       "Üniversitenin Araştırma ve Yayıma Verdiği Önem", "Programın İçeriği",
@@ -68,23 +82,39 @@ def recommendation():
 
     similar_rows = data.loc[similar_indices]
 
+    # Get the selected countries from the form
+    selected_countries = request.form.getlist('country')
+
+    # Filter the merged_data based on the selected countries
+    if selected_countries:
+        merged_data_filtered = merged_data[merged_data['Country'].isin(selected_countries)]
+    else:
+        merged_data_filtered = merged_data
+
+    # Prepare the filtered_data for further processing or usage
+    filtered_data = merged_data_filtered.copy()
     # Get the university name from the most similar row
     most_similar_university = similar_rows.iloc[0]['Hangi ünide okuyorsunuz? YÜKSEKLİSANS']
-    cluster = find_similar_universities(most_similar_university)
+    cluster = find_similar_universities(most_similar_university, merged_data_filtered)
     cluster_df = pd.DataFrame(cluster)
-    cluster_df.columns=['Universities']
+    cluster_df.columns = ['Universities']
+
+    # Filter the cluster universities based on selected countries
+    if selected_countries:
+        cluster_filtered = cluster_df[cluster_df['Universities'].isin(merged_data_filtered['University'])]
+    else:
+        cluster_filtered = cluster_df
+
     # Prepare the result message
-    result_message = f"<h3>Universities:</h3>{cluster_df.to_html()}"
+    result_message = f"<h3>Universities:</h3>{cluster_filtered.to_html()}"
 
-    return render_template('index.html', result=result_message)
+    return render_template('index.html', result=result_message, merged_data=merged_data_json,
+                           unique_countries=unique_countries)
 
 
-def find_similar_universities(university):
-    # Read the CSV file containing the universities and their cluster information
-    df_mergedData = pd.read_csv('merged_data.csv')
+def find_similar_universities(university, datav1):
 
-    # Find the row with the matching university in the specified CSV file
-    matched_row = df_mergedData[df_mergedData['University'] == university]
+    matched_row = datav1[datav1['University'] == university]
 
     if matched_row.empty:
         print(f"No matching university found: {university}")
@@ -94,10 +124,10 @@ def find_similar_universities(university):
     matched_cluster_id = matched_row['Cluster'].values[0]
 
     # Find all universities with the same cluster ID
-    unis = df_mergedData[df_mergedData['Cluster'] == matched_cluster_id]['University'].tolist()
+    unis = datav1[datav1['Cluster'] == matched_cluster_id]['University'].tolist()
 
     return unis
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
