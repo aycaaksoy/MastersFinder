8f961032512790064ed80f451e8d5d0e9d2ca644
Ayça Aksoy <aycaaksoy2000@gmail.com>
2023-05-26 11:17:55 +0300

    filtering works (multiple countries CAN be chosen)

--- app.py
+++ app.py
@@ -34,16 +34,27 @@ def recommendation():
     q7 = int(request.form['q7'])
 
     # Get the selected countries from the form
-    selected_countries = request.form.getlist('country')
+    selected_countries = request.form.getlist('selectedCountries')
+    merged_data_filtered = pd.DataFrame()
 
     # Filter the merged_data based on the selected countries
-    if selected_countries:
-        merged_data_filtered = merged_data[merged_data['Country'].isin(selected_countries)]
-    else:
-        merged_data_filtered = merged_data
+    if selected_countries[0] == '':
+        merged_data_filtered = merged_data.copy()
 
-    # Prepare the filtered_data for further processing or usage
-    filtered_data = merged_data_filtered.copy()
+    elif selected_countries[0] == 'allCountries':
+        merged_data_filtered = merged_data.copy()
+
+    elif "," in selected_countries[0]:
+        # Split the selected_countries string by commas to obtain individual country names
+        countries_list = [country.strip() for country in selected_countries[0].split(',')]
+        for country in countries_list:
+            # Filter the merged_data DataFrame for each country
+            filtered_data = merged_data[merged_data['Country'] == country]
+            # Concatenate the filtered data with the merged_data_filtered DataFrame
+            merged_data_filtered = pd.concat([merged_data_filtered, filtered_data])
+
+    else:
+        merged_data_filtered = merged_data[merged_data['Country'].isin(selected_countries)]
 
     user_input = {
         "Hangi bölümde okuyorsunuz? LİSANS": major,
@@ -82,48 +93,32 @@ def recommendation():
 
     similar_rows = data.loc[similar_indices]
 
-    # Get the selected countries from the form
-    selected_countries = request.form.getlist('country')
-
-    # Filter the merged_data based on the selected countries
-    if selected_countries:
-        merged_data_filtered = merged_data[merged_data['Country'].isin(selected_countries)]
-    else:
-        merged_data_filtered = merged_data
-
-    # Prepare the filtered_data for further processing or usage
-    filtered_data = merged_data_filtered.copy()
     # Get the university name from the most similar row
     most_similar_university = similar_rows.iloc[0]['Hangi ünide okuyorsunuz? YÜKSEKLİSANS']
     cluster = find_similar_universities(most_similar_university, merged_data_filtered)
     cluster_df = pd.DataFrame(cluster)
-    cluster_df.columns = ['Universities']
-
-    # Filter the cluster universities based on selected countries
-    if selected_countries:
-        cluster_filtered = cluster_df[cluster_df['Universities'].isin(merged_data_filtered['University'])]
-    else:
-        cluster_filtered = cluster_df
 
     # Prepare the result message
-    result_message = f"<h3>Universities:</h3>{cluster_filtered.to_html()}"
+    result_message = f"<h3>Universities:</h3>{cluster_df.to_html()}"
 
     return render_template('index.html', result=result_message, merged_data=merged_data_json,
                            unique_countries=unique_countries)
 
 
 def find_similar_universities(university, datav1):
-
-    matched_row = datav1[datav1['University'] == university]
-
-    if matched_row.empty:
-        print(f"No matching university found: {university}")
+    if datav1.empty:
+        print("No data available")
         return []
 
-    # Get the cluster ID of the matched university
+    matched_row = merged_data[merged_data['University'] == university]
+    if matched_row.empty:
+        print(f"No matching universities found for the selected countries")
+        return []
+
+    # Get the cluster IDs of the matched universities
     matched_cluster_id = matched_row['Cluster'].values[0]
 
-    # Find all universities with the same cluster ID
+    # Find all universities with the same cluster IDs
     unis = datav1[datav1['Cluster'] == matched_cluster_id]['University'].tolist()
 
     return unis
